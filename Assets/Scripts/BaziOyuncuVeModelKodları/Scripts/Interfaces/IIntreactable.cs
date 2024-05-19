f3b12be31058510fac6c17dbf06ec194caa13060
Arasberk <arasberkyildirim@hotmail.com>
2024-05-20 02:42:16 +0300

    Maze Map Added

--- Assets/Scripts/BaziOyuncuVeModelKodları/Scripts/Interfaces/IIntreactable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IIntreactable
-{
-    public void Intreact(KovalayanDusman ajan, float amount);
-}
